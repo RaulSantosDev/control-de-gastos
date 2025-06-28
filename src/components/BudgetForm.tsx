8423a52bc378f1ecafcc277deffa668f62824d11
RaulSantosDev <mrpucky27@gmail.com>
2025-06-27 22:23:27 -0600

    Validando que el presupuesto sea mayor que cero

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,46 @@
+import {useState, useMemo} from "react"
+
+export default function BudgetForm() {
+
+    const [ budget, setBudget ] = useState(0)
+
+    const handleChange = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+    return (
+        <form action="" className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className=" text-4xl text-blue-600 font-bold text-center">
+                    Definir Presupuesto
+                </label>
+
+                <input 
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />                
+
+            </div>
+
+            <input 
+                type="submit"
+                value="Definir Presupuesto"
+                className=" bg-blue-700 hover:bg-blue-600 cursor-pointer text-white p-2 rounded-lg w-full font-black uppercase disabled:opacity-50"
+                placeholder="100, 500, 1000"
+                disabled={isValid}
+            />
+
+        </form>
+
+        
+    )
+}
