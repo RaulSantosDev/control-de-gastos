2f11eae998e933a4d13a06567d7a8e06233a2fa3
RaulSantosDev <mrpucky27@gmail.com>
2025-08-25 08:33:28 -0600

    Mostrando los gastos

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import type { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProops = {
     expense: Expense
@@ -7,8 +9,22 @@ type ExpenseDetailProops = {
 export default function ExpenseDetail({expense} : ExpenseDetailProops) {
 
   return (
-    <div>
-      <span>{expense.expenseName}</span> <span>{expense.amount}</span>
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div>
+                
+      </div>
+      
+      <div className="flex justify-between">
+
+        <p className="font-black text-2xl">{expense.expenseName}</p> 
+
+        <p className="text-slate-600 text-sm">{ formatDate(expense.date!.toString())}</p>
+
+      </div>
+
+      <AmountDisplay
+        amount={expense.amount}
+      />
     </div>
   )
 }
