25111c7215fa802e4a2e085fcfc4e78faa32cb0a
RaulSantosDev <mrpucky27@gmail.com>
2025-08-28 11:41:50 -0600

    Agregangdo la iteración de Swipe a los gastos para Editar y Eliminar

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -3,6 +3,14 @@ import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
 import { categories } from "../data/data"
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css'
 
 type ExpenseDetailProops = {
     expense: Expense
@@ -12,29 +20,57 @@ export default function ExpenseDetail({expense} : ExpenseDetailProops) {
 
   const categoryInfo = useMemo(()=> categories.filter( category => category.id === expense.category)[0] ,[expense])
 
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction
+        onClick={() => {}}
+      >
+        Actualizar
+      </SwipeAction>
+    </LeadingActions>
+  )
+  const trealingActions = () => (
+    <TrailingActions>
+      <SwipeAction
+        onClick={() => {}}
+        destructive={true}
+      >
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  )
+
   return (
-    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
-      <div>
-        <img 
-          src={`/icono_${categoryInfo.icon}.svg`} 
-          alt="icono gasto" 
-          className="w-20"
-        />
-      </div>
-      
-      <div className=" flex-1 space-y-2">
+    < SwipeableList >
+      < SwipeableListItem 
+        maxSwipe={30}
+        leadingActions={leadingActions()}
+        trailingActions={trealingActions()}
+      >
+        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+          <div>
+            <img 
+              src={`/icono_${categoryInfo.icon}.svg`} 
+              alt="icono gasto" 
+              className="w-20"
+            />
+          </div>
+          
+          <div className=" flex-1 space-y-2">
 
-        <p className="text-sm font-bold uppercase text-slate-500"> {categoryInfo.name }</p>
+            <p className="text-sm font-bold uppercase text-slate-500"> {categoryInfo.name }</p>
 
-        <p className="font-black text-2xl">{expense.expenseName}</p> 
+            <p className="font-black text-2xl">{expense.expenseName}</p> 
 
-        <p className="text-slate-600 text-sm">{ formatDate(expense.date!.toString())}</p>
+            <p className="text-slate-600 text-sm">{ formatDate(expense.date!.toString())}</p>
 
-      </div>
+          </div>
 
-      <AmountDisplay
-        amount={expense.amount}
-      />
-    </div>
+          <AmountDisplay
+            amount={expense.amount}
+          />
+        </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
